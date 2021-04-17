8cc541c44fc8ce691d1f8063b49de7231dbf3413
Mayank12899 <srivastavamayank679@gmail.com>
2021-04-17 18:44:00 +0530

    Added Beanstalk URL

--- src/components/global/axios.js
+++ src/components/global/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://http://Foodpalbackend-env.eba-kdguhvqc.ap-south-1.elasticbeanstalk.com",
+  baseURL: "http://Foodpalbackend-env.eba-wsvaa3rp.ap-south-1.elasticbeanstalk.com",
   timeout: 1000,
 });
